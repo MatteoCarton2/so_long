882895b1ee4c2b2012966eefe5ade63d60c5320e
Mattéo <mcarton@student.s19.be>
2025-04-08 22:32:59 +0200

    .ber

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:16:17 by mcarton           #+#    #+#             */
-/*   Updated: 2025/04/08 22:30:39 by mcarton          ###   ########.fr       */
+/*   Updated: 2025/04/08 22:32:55 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ int	check_extension(char *filename)
 	int	fd;
 
 	length = ft_strlen(filename);
-	if (length <= 4)
+	if (length < 4)
+		handle_error("Invalid map file");
+	if (length == 4)
 		handle_error("Invalid map file");
 	if (ft_strncmp(filename + (length - 4), ".ber", 4) != 0)
 		handle_error("File extension must be .ber");
+	
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		handle_error("Cannot open file");
