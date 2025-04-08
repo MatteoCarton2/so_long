3b8e63fece16be42a48718f55fd01180fa3a3cb1
Mattéo <mcarton@student.s19.be>
2025-04-08 23:31:34 +0200

    enfinnnn

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:13:09 by mcarton           #+#    #+#             */
-/*   Updated: 2025/04/08 23:10:28 by mcarton          ###   ########.fr       */
+/*   Updated: 2025/04/08 23:30:50 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ int	main(int argc, char **argv)
 	if (validate_map(argv[1], &map) == 0)
 		exit(EXIT_FAILURE);
 	if (check_path(&map) == 0)
+	{
+		free_map_resources(&map, map.height, NULL, -1);
 		handle_error("No valid path found");
+	}
 	if (init_game(&game, &map) == 0)
 		handle_error("Failed to initialize game");
 	return (0);
